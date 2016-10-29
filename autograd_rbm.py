5fb6a3a2df060720fcc3b77479f6e2978f07a19a
lishenggao <answergls@gmail.com>
2016-10-29 17:49:41 -0400

    refactor out session related.

--- autograd_rbm.py
+++ autograd_rbm.py
@@ -32,7 +32,6 @@ class RBM(object):
                 initializer=tf.constant_initializer(0.0))
 
         self.params = [self.weights, self.vbias, self.hbias]
-        self.sess = get_session()
 
     def compute_up(self, vis):
         hid_p = tf.nn.sigmoid(tf.matmul(vis, self.weights) + self.hbias)
@@ -41,7 +40,7 @@ class RBM(object):
     def compute_down(self, hid):
         vis_p = tf.nn.sigmoid(tf.matmul(hid, tf.transpose(self.weights)) + self.vbias)
         return vis_p
-
+    
     def sample(self, ps):
         rand_uniform = tf.random_uniform(ps.get_shape().as_list(), 0, 1)
         samples = tf.to_float(rand_uniform < ps)
@@ -49,7 +48,6 @@ class RBM(object):
 
     def free_energy(self, vis_samples):
         """Compute the free energy defined on visibles.
-
         return: free energy of shape: [batch_size, 1]
         """
         vbias_term = tf.matmul(vis_samples, self.vbias, transpose_b=True)
@@ -69,7 +67,6 @@ class RBM(object):
         
     def cd(self, vis, k):
         """Contrastive Divergence.
-
         params: vis is treated as vis_samples.
         """
         def cond(x, vis_p, vis_samples):
@@ -89,19 +86,12 @@ class RBM(object):
         else:
             recon_vis_p, recon_vis_samples = self.cd(vis, cd_k)
 
-        # treat recon_vis_samples as constant during gradient comp
-        recon_vis_samples = tf.stop_gradient(recon_vis_samples)
-
         # use two reduce mean because vis and pst_chain could have different batch_size
         cost = (tf.reduce_mean(self.free_energy(vis))
                 - tf.reduce_mean(self.free_energy(recon_vis_samples)))
 
-        updates = []
-        if persistent_vis is not None:
-            updates.append(persistent_vis.assign(recon_vis_samples))
-
         loss = self.l2_loss_function(vis)
-        return loss, cost, updates
+        return loss, cost, recon_vis_samples
 
     def l2_loss_function(self, vis):
         recon_vis_p, _ = self.vhv(vis)
@@ -110,88 +100,29 @@ class RBM(object):
         total_loss = tf.reduce_sum(tf.square(vis - recon_vis_p), dims[1:])
         return tf.reduce_mean(total_loss)
 
-    def train(self, train_xs, lr, num_epoch, batch_size, use_pcd, cd_k, output_dir):
-        vis_shape = train_xs.shape[1:]    # shape of single image
-        batch_shape = (batch_size,) + vis_shape
-        num_batches = len(train_xs) / batch_size
-        assert num_batches * batch_size == len(train_xs)
-
-        # graph related definitions
-        ph_vis = tf.placeholder(tf.float32, batch_shape, name='vis_input')
-        ph_lr = tf.placeholder(tf.float32, (), name='lr')
-        if use_pcd:
-            persistent_vis = tf.get_variable(
-                'persistent_vis', shape=batch_shape,
-                initializer=tf.random_uniform_initializer(0, 1))
-        else:
-            persistent_vis = None
-            
-        loss, cost, updates = self.get_loss_updates(ph_lr, ph_vis, persistent_vis, cd_k)
-        opt = tf.train.GradientDescentOptimizer(ph_lr)
-        train_step = opt.minimize(cost)
-
-        with self.sess.as_default():
-            # merged = tf.merge_all_summaries()
-            train_writer = tf.train.SummaryWriter('./train', self.sess.graph)
-            tf.initialize_all_variables().run()
-
-            for i in range(num_epoch):
-                t = time.time()
-                np.random.shuffle(train_xs)
-                loss_vals = np.zeros(num_batches)
-                for b in range(num_batches):
-                    batch_xs = train_xs[b * batch_size:(b+1) * batch_size]
-
-                    loss_vals[b], _, _ = self.sess.run(
-                        [loss, train_step, updates], feed_dict={ ph_vis: batch_xs, ph_lr: lr })
-                print 'Train Loss:', loss_vals.mean()
-                print '... Time took:', time.time() - t
-                if output_dir is not None:
-                    saver = tf.train.Saver()
-                    save_path = saver.save(
-                        self.sess,
-                        os.path.join(output_dir, '%s-epoch%d.ckpt' % (self.name, i)))
-                    print 'Model saved to:', save_path
-                    prob_imgs, _ = self.sample_from_rbm(100, 1000)
-                    img_path = os.path.join(output_dir, 'epoch%d-plot.png' % i)
-                    vis_weights(prob_imgs.T, 10, 10, (28, 28), img_path)
-                    params = self.get_model_parameters()
-                    params_vis_path = os.path.join(output_dir, 'epoch%d-filters.png' % i)
-                    vis_weights(params['weights'][:,:100], 10, 10, (28, 28), params_vis_path)
-
-    def load_model(self, model_path):
-        with self.sess.as_default():
-            tf.initialize_all_variables().run()
-            saver = tf.train.Saver()
-            saver.restore(self.sess, model_path)
-        print 'Model loaded from:', model_path
-
     def get_model_parameters(self):
-        with self.sess.as_default():
-            return {
-                'weights': self.weights.eval(),
-                'vbias': self.vbias.eval(),
-                'hbias': self.hbias.eval()
-            }
+        return {
+            'weights': self.weights,
+            'vbias': self.vbias,
+            'hbias': self.hbias
+        }
 
-    def sample_from_rbm(self, num_examples, num_steps, init=None):
-        num_steps_holder = tf.placeholder(tf.int32, ())
-        vis = tf.placeholder(tf.float32, (num_examples, self.num_vis))
-
+    def sample_from_rbm(self, num_steps, num_examples, vis):
         def cond(x, vis_p, vis_samples):
-            return tf.less(x, num_steps_holder)
+            return tf.less(x, num_steps)
 
         def body(x, vis_p, vis_samples):
             vis_p, vis_samples = self.vhv(vis_samples)
             return x+1, vis_p, vis_samples
 
-        if init is None:
-            init = np.random.normal(0, 1, (num_examples, self.num_vis))
-
+        _, prob_imgs, sampled_imgs = tf.while_loop(cond, body, [0, vis, vis], back_prop=False)
+        
+        '''
         with self.sess.as_default():
             _, prob_imgs, sampled_imgs = self.sess.run(
                 tf.while_loop(cond, body, [0, vis, vis], back_prop=False),
                 feed_dict={num_steps_holder: num_steps, vis: init})
+        '''
         return prob_imgs, sampled_imgs
 
 
