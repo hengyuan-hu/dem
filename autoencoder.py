2531d31ecb9c0d5f02e4a87bda06fef84995466a
hengyuan-hu <kevin.huhy@gmail.com>
2017-01-23 20:08:42 -0500

    add AutoEncoder class to handle training, splitting and saving models.

---
+++ autoencoder.py
@@ -0,0 +1,131 @@
+# import tensorflow as tf
+# import numpy as np
+import os
+import keras
+from keras.layers import Input # , Activation
+# from keras.layers import Convolution2D as Conv2D
+# from keras.layers import Deconvolution2D as Deconv2D
+# from keras.layers.noise import GaussianNoise
+# from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.utils.visualize_util import plot
+from keras.callbacks import LearningRateScheduler
+
+import utils
+
+
+def _build_model(x_shape, encode_fn, use_noise, decode_fn, weights_file):
+    assert encode_fn is not None or decode_fn is not None, \
+        'At least provide one function to build the model.'
+    x = Input(x_shape)
+    y = x
+    if encode_fn:
+        y = encode_fn(x, use_noise)
+    if decode_fn:
+        y = decode_fn(y)
+    model = Model(x, y)
+    if weights_file:
+        assert os.path.exists(weights_file), '%s does not exist' % weights_file
+        model.load_weights(weights_file)
+        print 'Model loaded from %s' % weights_file
+    return Model(x, y)
+
+
+_ENCODER_WEIGHTS_FILE = 'encoder.h5'
+_DECODER_WEIGHTS_FILE = 'decoder.h5'
+_AE_WEIGHTS_FILE = 'ae.h5'
+
+
+class AutoEncoder(object):
+    """Wrapper class for autoencoder."""
+    def __init__(self, dataset, encode_fn, decode_fn, folder):
+        # self.x_shape = x_shape
+        self.dataset = dataset
+        self.x_shape = self.dataset.x_shape
+        self.encode_fn = encode_fn
+        self.decode_fn = decode_fn
+        self.folder = folder
+        self.history = {}
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+
+    def build_models(self, weights_folder=None):
+        if weights_folder:
+            encoder_weights = os.path.join(weights_folder, _ENCODER_WEIGHTS_FILE)
+            decoder_weights = os.path.join(weights_folder, _DECODER_WEIGHTS_FILE)
+            ae_weights = os.path.join(weights_folder, _AE_WEIGHTS_FILE)
+        else:
+            encoder_weights = None
+            decoder_weights = None
+            ae_weights = None
+        self.ae = _build_model(
+            self.x_shape, self.encode_fn, True, self.decode_fn, ae_weights)
+        self.encoder = _build_model(
+            self.x_shape, self.encode_fn, False, None, encoder_weights)
+        self.code_shape = self.encoder.get_output_shape_at(-1)[1:]
+        self.decoder = _build_model(
+            self.code_shape, None, False, self.decode_fn, decoder_weights)
+
+    def train(self, batch_size, num_epoch, lr_schedule):
+        opt = keras.optimizers.SGD(lr=lr_schedule(0), momentum=0.9, nesterov=True)
+        callback_list = [LearningRateScheduler(lr_schedule)]
+        self.ae.compile(optimizer=opt, loss='mse')
+        history = self.ae.fit(
+            self.dataset.train_xs, self.dataset.train_xs,
+            nb_epoch=num_epoch,
+            batch_size=batch_size,
+            validation_data=(self.dataset.test_xs, self.dataset.test_xs),
+            shuffle=True, callbacks=callback_list)
+        self.history = history.history
+
+    def log(self):
+        if self.history:
+            utils.log_keras_history(
+                self.history, os.path.join(self.folder, 'log.txt'))
+        else:
+            print 'Not trained yet, no training history to log.'
+
+    def save_models(self):
+        plot_file = os.path.join(self.folder, 'graph.png')
+        plot(self.ae, to_file=plot_file, show_shapes=True)
+        print 'model graph is plotted and stored at %s' % plot_file
+
+        ae_weights_file = os.path.join(self.folder, _AE_WEIGHTS_FILE)
+        encoder_weights_file = os.path.join(self.folder, _ENCODER_WEIGHTS_FILE)
+        decoder_weights_file = os.path.join(self.folder, _DECODER_WEIGHTS_FILE)
+
+        self.ae.save_weights(ae_weights_file)
+        print 'ae saved in %s' % ae_weights_file
+
+        ae_weights = self.ae.get_weights()
+        encoder_num_layers = len(self.encoder.get_weights())
+        decoder_num_layers = len(self.decoder.get_weights())
+        assert len(ae_weights) == encoder_num_layers + decoder_num_layers, \
+            'Fail to split ae into encoder and decoder, num layer mismatch.'
+        self.encoder.set_weights(ae_weights[:encoder_num_layers])
+        self.decoder.set_weights(ae_weights[encoder_num_layers:])
+        self.encoder.save_weights(encoder_weights_file)
+        self.decoder.save_weights(decoder_weights_file)
+        print 'encoder and decoder saved'
+
+    def test_models(self, vis_fn):
+        rows = 10; cols = 10
+        init = self.dataset.test_xs[:rows * cols]
+        ae_recon = self.ae.predict(init)
+        ed_recon = self.decoder.predict(self.encoder.predict(init))
+        vis_fn(init, rows, cols, os.path.join(self.folder, 'test_input.png'))
+        vis_fn(ae_recon, rows, cols,
+               os.path.join(self.folder, 'test_autoencoder_recon.png'))
+        vis_fn(ed_recon, rows, cols,
+               os.path.join(self.folder, 'test_encoder_decoder_recon.png'))
+
+    def encode(self, dataset_cls):
+        encoded_train_xs = self.encoder.predict(self.dataset.train_xs)
+        encoded_test_xs = self.encoder.predict(self.dataset.test_xs)
+        encoded_dataset = dataset_cls(encoded_train_xs, self.dataset.train_ys,
+                                      encoded_test_xs, self.dataset.test_xs)
+        return encoded_dataset
+
+
+if __name__ == '__main__':
+    pass
