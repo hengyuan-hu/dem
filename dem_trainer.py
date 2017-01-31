1f5d1529294221a72936ab1d6c68139368a2f48f
hengyuan-hu <kevin.huhy@gmail.com>
2017-01-30 20:47:08 -0500

    still an experimental dem trainer.

--- dem_trainer.py
+++ dem_trainer.py
@@ -15,11 +15,14 @@ class DEMTrainer(object):
         self.sampler = sampler
         self.sampler_generator = sampler_generator # TODO: factor out as train's param?
         self.vis_fn = vis_fn
-        self.x_shape = self.dataset.x_shape
         self.x_data = tf.placeholder(tf.float32, [None]+list(self.x_shape))
         self.x_model = tf.placeholder(tf.float32, [None]+list(self.x_shape))
         self.sess = sess
         self.log = []
+
+    @property
+    def x_shape(self):
+        return self.dataset.x_shape
 
     def train(self, lr, num_epoch, batch_size, folder):
         if folder and not os.path.exists(folder):
@@ -32,7 +35,14 @@ class DEMTrainer(object):
         opt = tf.train.GradientDescentOptimizer(lr)
         # opt = tf.train.AdamOptimizer(lr)
         train_step = opt.minimize(cost)
-        sample_op, sampler_updates = self.sampler.sample()
+        if hasattr(self.sampler, 'samples'):
+            # TODO: use a Sampler base class to improve this
+            # use persistent chains
+            sample_op, sampler_updates = self.sampler.sample()
+        else:
+            # does not use persistent chains
+            print '>>>>>>>> using cd, not pcd'
+            sample_op, sampler_updates = self.sampler.sample(self.x_data)
 
         # prevent tf.init from resetting encoder
         # K.tensorflow_backend._initialize_variables()
@@ -42,6 +52,7 @@ class DEMTrainer(object):
             print ('Before training: l1 weights sum: %s, bias sum: %s'
                    % (l1_weights[0].sum(), l1_weights[1].sum()))
 
+        # helper graph for computing free energy
         fe_x = tf.placeholder(tf.float32, [None]+list(self.x_shape))
         fe_op = self.dem.free_energy(fe_x)
 
@@ -51,23 +62,29 @@ class DEMTrainer(object):
             loss_vals = np.zeros(num_batches)
             for b in range(num_batches):
                 x_data = train_xs[b*batch_size : (b+1)*batch_size]
-                x_model, _ = self.sess.run([sample_op, sampler_updates])
+                if not hasattr(self.sampler, 'samples'):
+                    feed_dict = {self.x_data: x_data}
+                else:
+                    feed_dict = {}
+                x_model, _ = self.sess.run([sample_op, sampler_updates], feed_dict)
 
                 # fe_x_data = self.sess.run(fe_op, {fe_x: x_data})
                 # fe_x_model = self.sess.run(fe_op, {fe_x: x_model})
-                # print 'free energy\nx_data: %s;\nx_model: s' % (
-                #     fe_x_data[:5])# , fe_x_model[:5])
-                # print fe_x_data.shape# , fe_x_model.shape
-                # print x_data.max(), x_data.min()
+                # if b % 10  == 0:
+                #     print 'free energy\nx_data: %s;\nx_model: %s' % (
+                #         fe_x_data[:5], fe_x_model[:5])
 
                 feed_dict = {self.x_data: x_data, self.x_model: x_model}
                 loss_vals[b], _ = self.sess.run([loss, train_step], feed_dict)
-
-                # print '\tAccept rate:', self.sess.run([self.sampler.avg_accept_rate])
-                # print '\tStep size:', self.sess.run([self.sampler.stepsize])
+                # if b % 10 == 0:
+                #     print '\tAccept rate:', self.sess.run([self.sampler.avg_accept_rate])
+                #     print '\tStep size:', self.sess.run([self.sampler.stepsize])
 
             self.log.append('Epoch %d, Train Loss: %.4f' % (e+1, loss_vals.mean()))
             print self.log[-1]
+            weights_sum = self.sess.run(self.dem.weights)
+            weights_sum = weights_sum.sum()
+            print '\tweights sum:', weights_sum
             # print '\tTime Taken: %ss' % (time.time() - t)
             # print '\tAccept rate:', self.sess.run([self.sampler.avg_accept_rate])
             # print '\tStep size:', self.sess.run([self.sampler.stepsize])
@@ -77,15 +94,15 @@ class DEMTrainer(object):
                 print ('\tl1 weights sum: %s, bias sum: %s'
                        % (l1_weights[0].sum(), l1_weights[1].sum()))
 
-            if (e+1) % 1 == 0 and folder:
+            if (e+1) % 5 == 0 and folder:
                 samples = self._draw_samples()
                 samples_path = os.path.join(folder, 'samples-epoch%d.png' % (e+1))
                 chain_path = os.path.join(folder, 'pcd-chain-epoch%d.png' % (e+1))
                 # print 'saving imgs'
                 self._save_samples(samples, samples_path)
+                # self.dem.save_model(self.sess, folder, 'epoch_%d_' % e)
                 # self._save_samples(x_model, chain_path)
 
-        # self.sess.close()
     def dump_log(self, folder):
         path = os.path.join(folder, 'dem_train.log')
         with open(path, 'w') as f:
@@ -98,17 +115,21 @@ class DEMTrainer(object):
         sampler = self.sampler_generator()
         # K.tensorflow_backend._initialize_variables()
         utils.initialize_uninitialized_variables_by_keras()
+
         sample_op, sampler_updates = sampler.sample()
         for i in range(burnin):
-            samples, _, = self.sess.run([sample_op, sampler_updates])
+            self.sess.run([sample_op, sampler_updates])
+            # if i % 10 == 0:
+            #     print '\tAccept rate:', self.sess.run([sampler.avg_accept_rate])
+            #     print '\tStep size:', self.sess.run([sampler.stepsize])
 
         samples, _ = self.sess.run([sample_op, sampler_updates])
-        # print samples.max(), samples.min()
+        print 'in _draw_samples: samples min: %.4f, max: %.4f' \
+            % (samples.min(), samples.max())
         return samples
 
     def _save_samples(self, samples, img_path):
         batch_size = len(samples)
-        rows = int(math.sqrt(batch_size))
-        cols = rows
+        rows, cols = utils.factorize_number(batch_size)
         assert rows * cols == batch_size, '# of samples should be a square'
         self.vis_fn(samples, rows, cols, img_path)
