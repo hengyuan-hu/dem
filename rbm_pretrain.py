303af95f3e0662470ec116d2a5e3f26c68837336
hengyuan-hu <kevin.huhy@gmail.com>
2017-02-05 23:18:07 -0500

    add multi-stage traing and it kinda works.

--- rbm_pretrain.py
+++ rbm_pretrain.py
@@ -34,7 +34,56 @@ class RBMPretrainer(DEMTrainer):
         super(RBMPretrainer, self)._save_samples(samples, img_path)
 
 
-def pretrain(use_pcd, num_hid, num_epoch, batch_size, lr, cd_k):
+def create_cd_sampler_and_sampler_generator(rbm, cd_k, test_init):
+    if cd_k >= 10:
+        # when cd-k >= 10, use persistent sampler to generate samples,
+        # not reconstructions
+        test_init = None
+
+    def sampler_generator():
+        """default params to generate samplers for sampling, not training."""
+        return GibbsSampler(test_init, rbm, 1)
+
+    sampler = GibbsSampler(None, rbm, cd_k)
+    return sampler, sampler_generator
+
+
+def create_pcd_sampler_and_sampler_generator(rbm, cd_k, chain_shape):
+    def sampler_generator(cd_k=1):
+        """default params to generate samplers for sampling, not training."""
+        init_vals = np.random.normal(0.0, 1.0, chain_shape)
+        return GibbsSampler(init_vals, rbm, cd_k)
+
+    sampler = sampler_generator(cd_k)
+    return sampler, sampler_generator
+
+
+def pretrain(sess, rbm, dataset, decoder, train_config, vis_fn, parent_dir):
+    if train_config.use_pcd:
+        sampler, sampler_generator = create_pcd_sampler_and_sampler_generator(
+            rbm, train_config.cd_k, (train_config.batch_size,)+dataset.x_shape)
+    else:
+        sampler, sampler_generator = create_cd_sampler_and_sampler_generator(
+            rbm, train_config.cd_k, dataset.test_xs[:100])
+
+    trainer = RBMPretrainer(sess, dataset, rbm, decoder, sampler,
+                            sampler_generator, vis_fn)
+    rbm_dir = 'ptrbm_hid%d_%s' % (rbm.num_hid, str(train_config))
+    output_dir = os.path.join(parent_dir, rbm_dir)
+
+    train_config.dump_log(output_dir)
+    trainer.train(train_config, output_dir)
+    trainer.dump_log(output_dir)
+
+
+def multi_stage_pretrain(sess, rbm, dataset, decoder,
+                         train_configs, vis_fn, parent_dir):
+    for config in train_cofigs:
+        pretrain(sess, rbm, dataset, decoder, config, vis_fn, parent_dir)
+
+
+if __name__ == '__main__':
+    np.random.seed(666)
     sess = utils.create_session()
     K.set_session(sess)
 
@@ -47,46 +96,22 @@ def pretrain(use_pcd, num_hid, num_epoch, batch_size, lr, cd_k):
     encoded_dataset = Cifar10Wrapper.load_from_h5(
         os.path.join(ae_folder, 'encoded_cifar10.h5'))
     assert len(encoded_dataset.x_shape) == 1
-    num_vis = encoded_dataset.x_shape[0]
-    rbm = RBM(num_vis, num_hid, None)
 
-    if use_pcd:
-        sample_init_vals = np.random.normal(
-        0.0, 1.0, (batch_size,) + encoded_dataset.x_shape)
-    else:
-        sample_init_vals = encoded_dataset.test_xs[:100]
+    num_hid = 2000
+    rbm = RBM(encoded_dataset.x_shape[0], num_hid, None)
 
-    def sampler_generator(cd_k=1, init_vals=sample_init_vals):
-        # default params to generate samplers for sampling, not training
-        return GibbsSampler(init_vals, rbm, cd_k)
+    # train_config = utils.TrainConfig(
+    #     lr=0.1, batch_size=100, num_epoch=20, use_pcd=False, cd_k=1)
 
-    if use_pcd:
-        init_vals = np.random.normal(
-            0.0, 1.0, (batch_size,) + encoded_dataset.x_shape)
-    else:
-        init_vals = None
+    train_configs = [
+        utils.TrainConfig(lr=0.1, batch_size=100, num_epoch=100, use_pcd=False, cd_k=1),
+        utils.TrainConfig(lr=0.05, batch_size=100, num_epoch=100, use_pcd=False, cd_k=3),
+        utils.TrainConfig(lr=0.01, batch_size=100, num_epoch=200, use_pcd=False, cd_k=10),
+        utils.TrainConfig(lr=0.001, batch_size=100, num_epoch=500, use_pcd=True, cd_k=1),
+        utils.TrainConfig(lr=0.001, batch_size=100, num_epoch=500, use_pcd=True, cd_k=5),
+    ]
 
-    gibbs_sampler = sampler_generator(cd_k, init_vals)
-
-    trainer = RBMPretrainer(
-        sess, encoded_dataset, rbm, ae.decoder,
-        gibbs_sampler, sampler_generator, utils.vis_cifar10)
-
-    rbm_folder = 'pretrained_rbm_hid%d_lr%s_batch_%d' % (num_hid, lr, batch_size)
-    rbm_folder += '_pcd' if use_pcd else '_cd'
-    output_folder = os.path.join(ae_folder, rbm_folder)
-    trainer.train(lr, num_epoch, batch_size, output_folder)
-    trainer.dump_log(output_folder)
-
-
-def pretrain_with_cd():
-    return pretrain(False, 2000, 100, 100, 0.1, 1)
-
-
-def pretrain_with_pcd():
-    return pretrain(True, 2000, 500, 100, 0.01, 1)
-
-if __name__ == '__main__':
-    np.random.seed(666)
-    pretrain_with_cd()
-    # pretrain_with_pcd()
+    output_folder = os.path.join(ae_folder, 'multi_stage_pretrain')
+    for config in train_configs:
+        pretrain(sess, rbm, encoded_dataset, ae.decoder,
+                 config, utils.vis_cifar10, output_folder)
