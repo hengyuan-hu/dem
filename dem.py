fix gradient, and change to E(abs(x)) for loss balancing.

dem.py
@@ -7,6 +7,12 @@ def tf_norm(x):
     return tf.sqrt(tf.reduce_sum(tf.square(x)))
 
 
+def tf_mean_norm(x):
+    return tf.reduce_mean(tf.abs(x))
+    # return tf.sqrt(tf.reduce_sum(tf.square(x)))
+    # return tf.sqrt(tf.reduce_mean(tf.square(x)))
+
+
 class DEM(object):
     """Compositional model consists of encoder, assoc memory(rbm), decoder."""
     def __init__(self, ae, rbm, encoder=None, decoder=None):
@@ -76,7 +82,8 @@ class DEM(object):
         z = self.encoder(x)
         fe = tf.reduce_mean(self.rbm.free_energy(z))
         dfe_dz = tf.gradients(fe, z)[0]
-        grad_norm = tf_norm(dfe_dz)
+        grad_norm = tf_mean_norm(dfe_dz)
+        grad_norm = tf.stop_gradient(grad_norm)
         # grad_norm = tf.Print(grad_norm, ['fe grad_norm:', grad_norm])
         return fe / grad_norm
 
@@ -99,7 +106,8 @@ class DEM(object):
         x_recon = self.decoder(self.encoder(x))
         cost = tf.reduce_mean(tf.square(x_recon - x))
         dcost_dx_recon = tf.gradients(cost, x_recon)[0]
-        grad_norm = tf_norm(dcost_dx_recon)
+        grad_norm = tf_mean_norm(dcost_dx_recon)
+        grad_norm = tf.stop_gradient(grad_norm)
         # grad_norm = tf.Print(grad_norm, ['ae grad_norm:', grad_norm])
         return cost / grad_norm
 

real_dem_trainer.py
@@ -89,7 +89,7 @@ class DEMTrainer(object):
         opt_rbm = tf.train.GradientDescentOptimizer(
             train_config.lr).minimize(rbm_cost)
         opt_ae = tf.train.GradientDescentOptimizer(
-            train_config.lr * 0.01).minimize(ae_cost + encoder_fe_cost, var_list=ae_vars)
+            1e-8).minimize(ae_cost + encoder_fe_cost, var_list=ae_vars)
 
         if sampler.is_persistent:
             print '>>>>>>>> using pcd-%d' % train_config.cd_k
@@ -123,7 +123,7 @@ class DEMTrainer(object):
                 # # downward pass
                 # x_model = self.dem.decoder.predict(z_model)
 
-                # # check gradient magnitute
+                # check gradient magnitute
                 # grad_fe, grad_ae = self.sess.run(
                 #     [efc_grad_mean, aec_grad_mean],
                 #     {ae_x: x_data,
@@ -153,7 +153,7 @@ class DEMTrainer(object):
             if True:
                 el1_weights = self.dem.encoder.layers[1].get_weights()
                 dl1_weights = self.dem.decoder.layers[-2].get_weights()
-                print 'encoder L1 weights sum: %s, decoder L1 weights sum: %s' \
+                print '\tencoder L1 weights sum: %s, decoder L1 weights sum: %s' \
                     % (el1_weights[0].sum(), dl1_weights[0].sum())
 
             if (e+1) % 5 == 0: